d443f0efe24df10ea48b8fb12975a37476c4f56e
flyingpig-zzy <68273905+flyingpig-zzy@users.noreply.github.com>
2021-10-20 14:33:35 +0800

    fix

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -78,6 +78,8 @@
 
 <style lang='scss'>
 @import "assets/css/tutorial.scss";
+</style>
+<style>
 @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 </style>
 
